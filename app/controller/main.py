834c44e10cb353b98449d0a6947362cd19f6d982
SamSam <you@example.com>
2021-01-09 17:16:41 +0100

    add import es article

--- app/controller/main.py
+++ app/controller/main.py
@@ -1,7 +1,7 @@
 from flask import Flask,Blueprint, session, redirect, url_for, request,render_template
 from newsscrapper import fr
 from newsscrapper import en
-
+from newsscrapper import es
 
 bp = Blueprint('main', __name__)
 
